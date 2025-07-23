4c953b6f2f0bcb6840ec42a5a656a904f88a7f64
ArtemDema <artem100227dema@gmail.com>
2025-07-23 15:02:08 +0300

    edit model amd function

--- login_app/views.py
+++ login_app/views.py
@@ -1,42 +1,48 @@
 from .models import *
 import flask
 
-def render_register():
-    if flask.request.method == "POST":
-        name = flask.request.form["username"]
-        email = flask.request.form["email"]
-        user = User(
-            name = name,
-            email = email
-        )
-        DATABASE.session.add(user)
-        DATABASE.session.commit()
+# def render_register():
+#     if flask.request.method == "POST":
+#         password = flask.request.form["password"]
+#         email = flask.request.form["email"]
+#         user = User(
+#             password = password,
+#             email = email
+#         )
+#         DATABASE.session.add(user)
+#         DATABASE.session.commit()
         
 
-    return flask.redirect('/')
+#     return flask.redirect('/')
 
-def authorization():
-    if flask.request.method == "POST":
-        name = flask.request.form["name"]
-        email = flask.request.form["email"]
-        list_user = User.query.all()
-        for user in list_user:
-            if user.name == name and user.email == email:
-                flask_login.login_user(user)
+# def authorization():
+#     if flask.request.method == "POST":
+#         password = flask.request.form["password"]
+#         email = flask.request.form["email"]
+#         list_user = User.query.all()
+#         for user in list_user:
+#             if user.password == password and user.email == email:
+#                 flask_login.login_user(user)
 
-    return flask.redirect('/')
+#     return flask.redirect('/')
 
 def create_people():
     user1 = User(
-                name = "Alex1",
+                name = "Alex1.0",
+                last_name = "White1.0",
+                password = "Alex1",
                 email = "email1@gmail.com"
             )
     user2 = User(
-                name = "Alex2",
+                name = "Alex2.0",
+                last_name = "White2.0",
+                password = "Alex2",
                 email = "email2@gmail.com"
             )
     user3 = User(
-                name = "Alex3",
+                name = "Alex3.0",
+                last_name = "White3.0",
+                password = "Alex3",
                 email = "email3@gmail.com"
             )
     
