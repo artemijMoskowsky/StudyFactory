231b615b7d97d40ce0a33e05fecda98013dadd7f
Пользователь <olezhka82802@gmail.com>
2025-07-25 17:57:10 +0300

    added a home page layout

--- project/__init__.py
+++ project/__init__.py
@@ -3,7 +3,9 @@ from .settings import *
 from .db import *
 from .loadenv import *
 from .login_manager import *
+from core_app.app import *
 
 from login_app.app import *
 
 project.register_blueprint(blueprint = login_app)
+project.register_blueprint(blueprint = core_app)
--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,8 @@
 from login_app.app import login_app
 from login_app.views import render_login, render_reg
+from core_app.app import core_app
+from core_app.views import render_home
 
 login_app.add_url_rule("/login", view_func=render_login, methods = ["POST", "GET"])
 login_app.add_url_rule("/registration", view_func=render_reg, methods = ["POST", "GET"])
+core_app.add_url_rule("/", view_func=render_home, methods=["POST", "GET"])
