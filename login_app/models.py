598d1c3271eb205e34fdc4169e22c34e88930722
Artem <artemij.mosckowsky.01062008@gmail.com>
2025-08-02 13:34:14 +0300

    minor changes

--- login_app/models.py
+++ login_app/models.py
@@ -1,4 +1,5 @@
 import flask_login
+from sqlalchemy.ext.hybrid import hybrid_property
 from project.db import *
 
 class User(DATABASE.Model, flask_login.UserMixin):
@@ -7,3 +8,18 @@ class User(DATABASE.Model, flask_login.UserMixin):
     surname = DATABASE.Column(DATABASE.String)
     password = DATABASE.Column(DATABASE.String)
     email = DATABASE.Column(DATABASE.String)
+
+class Profile(DATABASE.Model):
+    # IMAGE_UPLOAD_PATH = 
+    id = DATABASE.Column(DATABASE.Integer, primary_key = True)
+    _image = DATABASE.Column(DATABASE.String)
+
+    @hybrid_property
+    def image(self):
+        return self._image
+    
+    @image.setter
+    def image(self, file):
+        
+        file += "123321"
+        self._image = file
