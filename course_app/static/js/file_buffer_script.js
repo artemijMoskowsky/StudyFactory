c0714605f52d0cc80af332d60cae87085670df4f
Artem Moskowsky <artemij.mosckowsky.01062008@gmail.com>
2025-08-18 08:44:59 +0300

    File viewer <back to page fix>

--- course_app/static/js/file_buffer_script.js
+++ course_app/static/js/file_buffer_script.js
@@ -1,10 +1,11 @@
 const fileInput = document.querySelector("#input-files");
 const fileBuffer = document.querySelector("#file-buffer");
 const filePlacer = document.querySelector("#file-placer");
+const fileLink = document.querySelector("meta[name='file-link']").content;
 
-fileInput.addEventListener("input", ()=>{
-    for (let child of filePlacer.children){
-        filePlacer.removeChild(child);
+function loadElements(){
+    while (filePlacer.children.length) {
+        filePlacer.removeChild(filePlacer.firstChild);
     }
     let dataTransfer = new DataTransfer;
     for (let file of fileBuffer.files){
@@ -13,8 +14,11 @@ fileInput.addEventListener("input", ()=>{
     for (let file of fileInput.files){
         dataTransfer.items.add(file);
     }
-    
+    // console.log(fileBuffer.files, fileInput.files, dataTransfer.files)
     fileBuffer.files = dataTransfer.files;
+    let clearList = new DataTransfer;;
+    fileInput.files = clearList.files;
+    console.log(fileBuffer.files)
 
     // let fileReader = new FileReader;
     // fileReader.onload = function () {
@@ -28,12 +32,36 @@ fileInput.addEventListener("input", ()=>{
     //     fileReader.readAsDataURL(file);
     // }
     for (let file of fileBuffer.files){
-        let fileBase = document.createElement("div")
+        let fileBase = document.createElement("div");
+        fileBase.classList.add("file-base");
         let fileImg = document.createElement("img");
+        if (file.type.startsWith("image")){
+           let fr = new FileReader;
+           fr.onload = function () {
+            fileImg.src = fr.result;
+           }
+           fr.readAsDataURL(file);
+        } else if (file.type.startsWith("video")){
+           
+        } else {
+            fileImg.src = fileLink;
+        }
+        fileImg.classList.add("file-img")
         let fileName = document.createElement("p");
-        fileName.textContent = `${file.name} ${Math.round(file.size/1024)} KB`
+        fileName.textContent = `${file.name}`;
+        fileName.classList.add("file-name");
+        let fileSize = document.createElement("p");
+        fileSize.classList.add("file-size");
+        fileSize.textContent = `${Math.round(file.size/1024)} KB`;
         fileBase.appendChild(fileImg);
         fileBase.appendChild(fileName);
+        fileBase.appendChild(fileSize);
         filePlacer.appendChild(fileBase);
     }
+}
+
+
+window.addEventListener("pageshow", (event)=>{
+    loadElements();
+    fileInput.addEventListener("input", loadElements);
 })
